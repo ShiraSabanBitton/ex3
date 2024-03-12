b9e4c6d38f7a19d9cf514f0c83f26f0d87491154
Shira Saban Biton <you@example.com>
2024-03-12 19:26:55 +0200

    added comments to the files

--- profile.js
+++ profile.js
@@ -1,12 +1,28 @@
+//Shira Saban Bitton - 316511658
+//Fida Rabah - 204647911
+//12/3/24
+//The file is a Node.js script that creates a web server using the Express.js framework.
+//It sets up routes to handle incoming HTTP requests, reads data from files, and renders dynamic content to be displayed on web pages.
+
+// This line imports the Express.js framework.
+//Express is a web application framework for Node.js that simplifies the process of building web applications and APIs by providing a robust set of features for handling HTTP requests, routing, middleware, and more.
 const express = require("express");
+//This line imports the built-in Node.js File System (fs) module.
+//This module provides functions for working with the file system, allowing you to read from and write to files on your computer.
 const fs = require("fs");
+//This line imports the built-in Node.js Path module.
+//This module provides utilities for working with file and directory paths.
+//It helps in constructing absolute paths and resolving relative paths.
 const path = require("path");
+//This line creates an instance of the Express application by calling the express() function.
+//This instance represents your web application and allows you to define routes, middleware, and other configurations.
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+//Listen on profile route and read the profile files accurding to the id that we get from the web
 app.get("/profile", (req, res) => {
   let id = req.query.id;
 
